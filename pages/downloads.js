404a536ff8657cef669e5fab9a56c633ed52e9f2
Evan Tahler <evantahler@gmail.com>
2017-06-06 13:40:44 -0700

    yarn

--- pages/downloads.js
+++ pages/downloads.js
@@ -6,10 +6,17 @@ import DocSection from './../components/elements/docSection.js'
 import Code from './../components/code.js'
 
 let npm = `# mkdir my_project && cd my_project
-npm install actionhero
+npm install actionhero --save
 ./node_modules/.bin/actionhero generate
 npm install
 npm start
+`
+
+let yarn = `# mkdir my_project && cd my_project
+yarn add actionhero
+yarn run actionhero generate
+yarn add ws fakeredis ioredis chai dirty-chai mocha standard
+yarn run actionhero
 `
 
 export default class extends React.Component {
@@ -20,6 +27,13 @@ export default class extends React.Component {
         icon: '/static/images/downloads.svg'
       }}>
         <Row>
+          <Col md={12}>
+            <br />
+            <br />
+            <p>ActionHero's source code licence is The V2 Apache Licence, and is distributed with the code.  ActionHero can be installed via a number of methods, but we recommend using NPM or Yarn.</p>
+          </Col>
+        </Row>
+        <Row>
           <Col md={6} style={{textAlign: 'center', padding: 50}}>
             <DocSection title='NPM' />
             <img src='/static/images/downloads/npm.svg' />
@@ -27,6 +41,15 @@ export default class extends React.Component {
             <br />
             <div style={{textAlign: 'left'}}>
               <Code language='bash'>{npm}</Code>
+            </div>
+          </Col>
+          <Col md={6} style={{textAlign: 'center', padding: 50}}>
+            <DocSection title='Yarn' />
+            <img height={100} src='/static/images/downloads/yarn.svg' />
+            <br />
+            <br />
+            <div style={{textAlign: 'left'}}>
+              <Code language='bash'>{yarn}</Code>
             </div>
           </Col>
           <Col md={6} style={{textAlign: 'center', padding: 50}}>
