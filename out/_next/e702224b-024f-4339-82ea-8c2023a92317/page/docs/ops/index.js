897fef354a6a4642c0fb7e925601c6093f0f7b68
Evan Tahler <evantahler@gmail.com>
2017-08-09 15:42:13 -0700

    do not check in OUT

--- out/_next/e702224b-024f-4339-82ea-8c2023a92317/page/docs/ops/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/docs/ops', function() {
-            var comp = module.exports=webpackJsonp([18],{1232:function(e,t,n){e.exports=n(1233)},1233:function(e,t,n){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=n(9),a=u(l),r=n(1),o=u(r),d=n(8),f=u(d),c=n(2),i=u(c),s=n(3),p=u(s),m=n(0),h=u(m),_=n(60),v=u(_),y=n(39),E=u(y),b=n(91),k=u(b),x=function(e){function t(){return(0,o.default)(this,t),(0,i.default)(this,(t.__proto__||(0,a.default)(t)).apply(this,arguments))}return(0,p.default)(t,e),(0,f.default)(t,[{key:"componentDidMount",value:function(){v.default.replace("/docs")}},{key:"render",value:function(){return h.default.createElement(k.default,null,h.default.createElement("div",{style:{textAlign:"center"}},h.default.createElement("h1",null,"You shouldn't be here!"),h.default.createElement("h2",null,"Try ",h.default.createElement(E.default,{href:"/docs"},h.default.createElement("a",null,"starting over in the Documentation secion")),".")))}}]),t}(h.default.Component);t.default=x}},[1232]);
-            return { page: comp.default }
-          })
-        
