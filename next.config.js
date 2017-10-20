6e0056a067d70f7806af4f3891675ba5938c658f
evan tahler <evantahler@gmail.com>
2017-10-20 15:50:38 -0700

    bring back static site config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/index': { page: '/index' },
+      '/community': { page: '/community' },
+      '/downloads': { page: '/downloads' },
+      '/get-started': { page: '/get-started' },
+      '/solutions': { page: '/solutions' },
+      '/terms': { page: '/terms' }
+    }
+  }
+}
