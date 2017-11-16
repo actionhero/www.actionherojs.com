514551943d4010840eb1a5d2490e4d46ebd0c0a7
evan tahler <evantahler@gmail.com>
2017-11-16 14:23:15 -0800

    remove comment

--- components/code.js
+++ components/code.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
-// import { github } from 'react-syntax-highlighter/dist/styles'
 import { prism } from 'react-syntax-highlighter/styles/prism'
 
 export default class extends React.Component {
