18c2e218a7ffc942132440d1297908a7c4a09bbd
Evan Tahler <evan.tahler@voom.flights>
2018-07-22 20:24:03 -0700

    add statig mapping for plugins

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
       '/downloads': { page: '/downloads' },
       '/get-started': { page: '/get-started' },
       '/solutions': { page: '/solutions' },
+      '/pugins': { page: '/pugins' },
       '/terms': { page: '/terms' }
     }
   }
