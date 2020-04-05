1c3371c13d97dc0447d28989213c842058ac7d0d
Evan Tahler <evantahler@gmail.com>
2020-04-05 12:35:36 -0700

    solutions page

---
+++ pages/solutions.tsx
@@ -0,0 +1,11 @@
+import { Container, Row, Col } from "react-bootstrap";
+import Page from "./../components/layouts/page";
+import SolutionsGrid from "./../components/solutionsGrid";
+
+export default function Solutions() {
+  return (
+    <Page>
+      <SolutionsGrid />
+    </Page>
+  );
+}
